30aba0a8a9d7f5665f99d5576e468a150423bca4
badac <badaclock@gmail.com>
2024-08-23 02:51:42 +0900

    settings: 파이어베이스 설정 추가 (#6)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // resolve: {
+  //   alias: {
+  //     '@src': path.resolve(__dirname, './src'),
+  //     '@assets': path.resolve(__dirname, './src/assets'),
+  //     '@components': path.resolve(__dirname, './src/components'),
+  //     '@server': path.resolve(__dirname, './server'),
+  //   },
+  // },
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'], // 여기서 babel-plugin 추가
+      },
+    }),
+    tsconfigPaths(),
+    visualizer(),
+  ],
+});
