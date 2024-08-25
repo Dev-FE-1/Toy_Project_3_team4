00a87311c3974089dd6c5de3f1b7c38a1fadd53e
badac <badaclock@gmail.com>
2024-08-25 16:00:32 +0900

    chore: firbase functions eslint 설정 (#7)

--- functions/.eslintrc.cjs
+++ functions/.eslintrc.cjs
@@ -14,12 +14,14 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
+    project: ["./tsconfig.json", "./tsconfig.dev.json"],
     sourceType: "module",
+    tsconfigRootDir: __dirname,
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
     "/generated/**/*", // Ignore generated files.
+    ".eslintrc.cjs",
   ],
   plugins: [
     "@typescript-eslint",
