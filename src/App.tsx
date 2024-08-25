848a9e1acee13cfa5cffce3fbc0f08e35de589d7
deun <devdeun@gmail.com>
2024-08-25 16:10:22 +0900

    feat: 라우터 설정 (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-import Navbar from '@/components/layout/Navbar/Navbar';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
-}
+import { PATH } from '@/constants/path';
+import RootLayout from '@/layouts/Root';
+import AddPostPage from '@/pages/AddPost';
+import HomePage from '@/pages/Home';
+import PlaylistPage from '@/pages/Playlist';
+import ProfilePage from '@/pages/Profile';
+import SearchPage from '@/pages/Search';
+
+const router = createBrowserRouter([
+  {
+    path: PATH.HOME,
+    children: [
+      {
+        path: '/',
+        element: <RootLayout />,
+        children: [
+          { index: true, element: <HomePage /> },
+          {
+            path: PATH.SEARCH,
+            element: <SearchPage />,
+          },
+          {
+            path: PATH.ADD_POST,
+            element: <AddPostPage />,
+          },
+          {
+            path: PATH.PLAYLIST,
+            element: <PlaylistPage />,
+          },
+          {
+            path: PATH.PROFILE,
+            element: <ProfilePage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
