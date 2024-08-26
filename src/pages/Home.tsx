fc16522c73e6b268d43b026c628e86a128d9118f
badac <badaclock@gmail.com>
2024-08-26 12:05:27 +0900

    feat: 모달 컴포넌트 생성
    - src/components/common/Modal.tsx 삭제
    - src/components/common/Modal/ 디렉토리 생성
    - src/components/common/Modal/Modal.tsx 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+
+import Modal from '@/components/common/Modal/Dialog';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <div>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Example Modal"
+        height="50%" // 높이를 조절할 수 있습니다
+        footer={
+          <>
+            <button onClick={() => setIsModalOpen(false)}>Cancel</button>
+            <button onClick={() => {}}>Confirm</button>
+          </>
+        }
+        animated={false}
+      >
+        <p>This is the content of the modal.</p>
+        <p>You can add any React components here.</p>
+      </Modal>
+      <button onClick={() => setIsModalOpen(true)}>Open Modal</button>
+      HomePage
+    </div>
+  );
 };
 
 export default HomePage;
