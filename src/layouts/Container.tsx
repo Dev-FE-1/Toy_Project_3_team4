9b89380da7ad438cd8f06d7f0ce84ddb10718451
deun <devdeun@gmail.com>
2024-08-27 09:34:45 +0900

    feat: Container 레이아웃 추가 (#17)
    - Settings 등 Navbar가 필요 없는 페이지에서 사용할 Container 레이아웃 추가
    - Root 레이아웃에서 Container 레이아웃 재사용

---
+++ src/layouts/Container.tsx
@@ -0,0 +1,28 @@
+import { css } from '@emotion/react';
+import { Outlet } from 'react-router-dom';
+
+import theme from '@/styles/theme';
+
+const ContainerLayout: React.FC = () => {
+  return (
+    <main css={mainStyle}>
+      <Outlet />
+    </main>
+  );
+};
+
+const mainStyle = css`
+  width: 100%;
+  max-width: ${theme.width.max};
+  height: 100vh;
+  padding: 0 16px;
+  margin: 0 auto;
+  background-color: ${theme.colors.white};
+
+  @media screen and (min-width: ${theme.width.max}) {
+    border-left: 1px solid ${theme.colors.lightGray};
+    border-right: 1px solid ${theme.colors.lightGray};
+  }
+`;
+
+export default ContainerLayout;
