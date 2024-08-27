41ad1b1435ae91ad78343e184e2fe7b091e8ebce
deun <devdeun@gmail.com>
2024-08-27 09:42:09 +0900

    feat: signin 페이지 로그인 기능 추가 (#17)

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,19 +1,32 @@
 import { css } from '@emotion/react';
 import { FcGoogle } from 'react-icons/fc';
 import { HiOutlineBolt } from 'react-icons/hi2';
+import { useNavigate } from 'react-router-dom';
 
+import { signInWithGoogle } from '@/api/firebaseAuth';
 import FullButton from '@/components/common/buttons/FullButton';
 import ChatBubble from '@/components/onboarding/ChatBubble';
 import OnboardingSwiper from '@/components/onboarding/OnboardingSwiper';
 import theme from '@/styles/theme';
 
 const SignInPage = () => {
+  const navigate = useNavigate();
+
+  const onSignIn = async () => {
+    try {
+      await signInWithGoogle();
+      navigate('/');
+    } catch (error) {
+      console.error('로그인에 실패했습니다', error);
+    }
+  };
+
   return (
-    <main css={mainStyle}>
+    <div css={mainStyle}>
       <OnboardingSwiper />
 
       <ChatBubble Icon={HiOutlineBolt} contents="5초만에 시작하기" customStyle={chatBubbleStyle} />
-      <FullButton styleType="signin" customStyle={buttonStyle}>
+      <FullButton styleType="signin" customStyle={buttonStyle} onClick={onSignIn}>
         <FcGoogle />
         Google로 계속하기
       </FullButton>
@@ -21,7 +34,7 @@ const SignInPage = () => {
         최초 로그인 시 계정을 생성하며, <br /> 플리의 <b>이용약관</b> 및 <b>개인정보처리방침</b>에
         동의하게 됩니다.
       </p>
-    </main>
+    </div>
   );
 };
 
@@ -29,17 +42,6 @@ const mainStyle = css`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100%;
-  max-width: ${theme.width.max};
-  height: 100vh;
-  padding: 0 16px;
-  margin: 0 auto;
-  background-color: ${theme.colors.white};
-
-  @media screen and (min-width: ${theme.width.max}) {
-    border-left: 1px solid ${theme.colors.lightGray};
-    border-right: 1px solid ${theme.colors.lightGray};
-  }
 
   .description {
     margin-top: 28px;
