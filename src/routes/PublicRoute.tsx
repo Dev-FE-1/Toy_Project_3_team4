a83463f8c4fc1992a90ce58827e44824872f91ee
deun <devdeun@gmail.com>
2024-08-27 09:36:29 +0900

    feat: PublicRoute, ProtectedRoute 추가 (#17)
    - 로그인 전에 다른 페이지에 접근하지 못하도록 ProtectedRoute 추가
    - 로그인 한 사용자가 signin 페이지에 접근하지 못하도록 PublicRoute 추가

---
+++ src/routes/PublicRoute.tsx
@@ -0,0 +1,16 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, Outlet } from 'react-router-dom';
+
+import { auth } from '@/api/firebaseApp';
+
+const PublicRoute: React.FC = () => {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return user ? <Navigate to="/" replace /> : <Outlet />;
+};
+
+export default PublicRoute;
