b656edfcf2307a33d7803a7e0c89055514e9bad6
deun <devdeun@gmail.com>
2024-08-27 09:40:23 +0900

    feat: 라우터에 Route 및 Layout 적용 (#17)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { PATH } from '@/constants/path';
+import ContainerLayout from '@/layouts/Container';
 import RootLayout from '@/layouts/Root';
 import AddPostPage from '@/pages/AddPost';
 import HomePage from '@/pages/Home';
 import PlaylistPage from '@/pages/Playlist';
 import ProfilePage from '@/pages/Profile';
 import SearchPage from '@/pages/Search';
+import SettingsPage from '@/pages/Settings';
 import SignInPage from '@/pages/SignIn';
+import ProtectedRoute from '@/routes/ProtectedRoute';
+import PublicRoute from '@/routes/PublicRoute';
 
 const router = createBrowserRouter([
   {
-    path: PATH.HOME,
+    element: <ContainerLayout />,
     children: [
-      { path: PATH.SIGN_IN, element: <SignInPage /> },
       {
-        path: '/',
+        path: PATH.SIGN_IN,
+        element: <PublicRoute />,
+        children: [{ index: true, element: <SignInPage /> }],
+      },
+      {
+        path: PATH.SETTINGS,
+        element: <ProtectedRoute />,
+        children: [{ index: true, element: <SettingsPage /> }],
+      },
+    ],
+  },
+  {
+    path: PATH.HOME,
+    element: <ProtectedRoute />,
+    children: [
+      {
         element: <RootLayout />,
         children: [
           { index: true, element: <HomePage /> },
