f8af35c906c214df5890153b1c0d807e0d81f135
deun <devdeun@gmail.com>
2024-08-29 10:33:25 +0900

    feat: 검색어를 입력하면 tab 추가 (#54)

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,13 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
+import { HiOutlinePencil, HiOutlineUser } from 'react-icons/hi2';
 import { useNavigate } from 'react-router-dom';
 
+import TabContent from '@/components/common/tabs/TabContent';
+import TabMenu from '@/components/common/tabs/TabMenu';
 import BackHeader from '@/components/layout/header/BackHeader';
 import { useDebounce } from '@/hooks/useDebounce';
+
+const tabs = [
+  { id: 'post', label: '포스트', icon: <HiOutlinePencil /> },
+  { id: 'user', label: '계정', icon: <HiOutlineUser /> },
+];
 
 const SearchPage = () => {
   const navigate = useNavigate();
   const { value: searchTerm, debouncedValue: debouncedSearchTerm, onChange } = useDebounce();
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
 
   const handleBackClick = () => {
     navigate(-1);
@@ -23,7 +32,16 @@ const SearchPage = () => {
   return (
     <>
       <BackHeader onBackClick={handleBackClick} showSearch onSearchChange={onChange} />
-      <div>SearchPage</div>
+      {searchTerm && (
+        <TabMenu activeTabId={activeTab} tabs={tabs} onTabChange={setActiveTab}>
+          <TabContent id="post" activeTabId={activeTab}>
+            <div>포스트</div>
+          </TabContent>
+          <TabContent id="user" activeTabId={activeTab}>
+            <div>유저</div>
+          </TabContent>
+        </TabMenu>
+      )}
       {searchTerm && <p>{searchTerm}</p>}
       {debouncedSearchTerm && <p>{debouncedSearchTerm}</p>}
     </>
