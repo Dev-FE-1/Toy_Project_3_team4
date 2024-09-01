4b619ac93379799aed8d3eb9b8c7ae230db00e07
James <dawn147@snu.ac.kr>
2024-09-01 18:08:23 +0900

    feat: App.tsx에 새로운 페이지 경로 추가 (#46) - path.ts도 없데이트

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -2,6 +2,7 @@ export const PATH = {
   HOME: '/',
   SEARCH: '/search',
   ADD_POST: '/post/add',
+  SELECT_PLI: '/post/add/selectpli',
   PLAYLIST: '/playlist',
   PROFILE: '/profile/:userId',
   PROFILE_Edit: '/profile/:userId/edit',
