aa3874339bd15940b234d6648033adf152db24f5
James <dawn147@snu.ac.kr>
2024-09-02 23:50:05 +0900

    style: 모달 관련 로직을 Navbar에서 NavButtonItem으로 옮김 (#46)

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { css } from '@emotion/react';
 import { CiSquarePlus } from 'react-icons/ci';
@@ -7,11 +7,8 @@ import {
   HiMagnifyingGlass,
   HiOutlineRectangleStack,
   HiOutlineUserCircle,
-  HiLink,
 } from 'react-icons/hi2';
-import { useNavigate } from 'react-router-dom';
 
-import Modal from '@/components/common/Modal';
 import NavButtonItem from '@/components/layout/navbar/NavButtonItem';
 import NavItem from '@/components/layout/navbar/NavItem';
 import { PATH } from '@/constants/path';
@@ -29,81 +26,18 @@ const Navbar = () => {
     }
   }, [currentUser]);
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const navigate = useNavigate();
-  const onClose = () => setIsModalOpen(false);
   return (
     <nav css={[navStyle, maxWidthStyle(false)]}>
       <ul className="nav-list">
         <NavItem Icon={HiOutlineHome} path={PATH.HOME} />
         <NavItem Icon={HiMagnifyingGlass} path={PATH.SEARCH} />
-        <NavButtonItem Icon={CiSquarePlus} onClick={() => setIsModalOpen(true)} />
+        <NavButtonItem Icon={CiSquarePlus} />
         <NavItem Icon={HiOutlineRectangleStack} path={PATH.PLAYLIST} />
         <NavItem Icon={HiOutlineUserCircle} path={PATH.PROFILE} userId={userId || ''} />
       </ul>
-      <Modal isOpen={isModalOpen} onClose={onClose} title="포스트 추가하기">
-        <div css={modalContentContainer}>
-          <div
-            onClick={() => {
-              onClose();
-              navigate(PATH.ADD_POST);
-            }}
-          >
-            <div className="icon-wrapper">
-              <HiLink />
-            </div>
-            링크로 동영상 추가
-          </div>
-          <div
-            onClick={() => {
-              onClose();
-              navigate(PATH.SELECT_PLI);
-            }}
-          >
-            <div className="icon-wrapper">
-              <HiOutlineRectangleStack />
-            </div>
-            플리에서 동영상 선택
-          </div>
-        </div>
-      </Modal>
     </nav>
   );
 };
-
-const modalContentContainer = css`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 12px;
-  align-self: stretch;
-  width: 343px;
-  margin: 24px 16px 32px;
-
-  & > div {
-    display: flex;
-    align-items: center;
-    height: 50px;
-    cursor: pointer;
-    width: 100%;
-
-    .icon-wrapper {
-      height: 50px;
-      width: 50px;
-      background-color: ${theme.colors.lightestGray};
-      border-radius: 16px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-right: 12px;
-
-      svg {
-        height: 18px;
-        width: 18px;
-      }
-    }
-  }
-`;
 
 const navStyle = css`
   height: 70px;
