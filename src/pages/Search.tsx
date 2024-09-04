5a99b8d78a1ada9ad577bc6a3e542579a5e20d98
deun <devdeun@gmail.com>
2024-09-04 21:54:00 +0900

    feat: 유저 검색 결과 출력 (#67)

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -8,8 +8,11 @@ import TabMenu from '@/components/common/tabs/TabMenu';
 import SearchHeader from '@/components/layout/header/SearchHeader';
 import { PostsTimeLine } from '@/components/post/PostsTimeline';
 import UserInfo from '@/components/user/UserInfo';
+import { useAuth } from '@/hooks/useAuth';
 import { useDebounce } from '@/hooks/useDebounce';
 import { useSearch } from '@/hooks/useSearch';
+import { useUserData } from '@/hooks/useUserData';
+import { userItemStyle } from '@/pages/Follow';
 import { PostModel } from '@/types/post';
 import { UserModel } from '@/types/user';
 
@@ -22,6 +25,8 @@ const SearchPage = () => {
   const { value: searchTerm, debouncedValue: debouncedSearchTerm, onChange } = useDebounce();
   const [activeTab, setActiveTab] = useState(tabs[0].id);
   const { data: searchResults } = useSearch(activeTab as SearchType, debouncedSearchTerm);
+  const currentUser = useAuth();
+  const { toggleFollow, isFollowing } = useUserData(currentUser?.uid || null);
 
   const isUserModel = (result: UserModel | PostModel): result is UserModel => {
     return 'displayName' in result;
@@ -37,11 +42,20 @@ const SearchPage = () => {
           </TabContent>
           <TabContent id="user" activeTabId={activeTab}>
             {activeTab === 'user' &&
-              searchResults
-                ?.filter(isUserModel)
-                .map((user) => (
-                  <UserInfo key={user.userId} name={user.displayName} url={user.photoURL} />
-                ))}
+              searchResults?.filter(isUserModel).map((user) => (
+                <div key={user.userId} css={userItemStyle}>
+                  <UserInfo
+                    name={user.displayName}
+                    url={user.photoURL}
+                    imageSize={'large'}
+                    additionalInfo={`팔로워 ${user.followers?.length || 0}명`}
+                    userId={user.userId}
+                    showFollowButton={user.userId !== currentUser?.uid}
+                    isFollowing={isFollowing(user.userId)}
+                    onFollowToggle={() => currentUser && toggleFollow(currentUser.uid, user.userId)}
+                  />
+                </div>
+              ))}
           </TabContent>
         </TabMenu>
       )}
