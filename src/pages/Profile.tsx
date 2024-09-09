a0f55ae25d48cf0a291a887f04af8abdbd49c47a
deun <devdeun@gmail.com>
2024-09-09 12:33:04 +0900

    feat: isLoading 문구 대신 Spinner 추가 (#126)

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -109,7 +109,11 @@ const ProfilePage: React.FC = () => {
   };
 
   if (!userData) {
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
   }
 
   const isOwnProfile = currentUser?.uid === userId;
@@ -137,7 +141,7 @@ const ProfilePage: React.FC = () => {
             ) : (
               userPosts.map((post) => <Post key={post.postId} post={post} id={post.postId} />)
             )}
-            {userPosts.length === 0 && (
+            {!loadingPosts && userPosts.length === 0 && (
               <EmptyMessage Icon={HiOutlinePencil}>아직 포스트가 없습니다</EmptyMessage>
             )}
           </TabContent>
@@ -155,9 +159,11 @@ const ProfilePage: React.FC = () => {
             ) : (
               <Playlists playlists={playlists || []} onPlaylistClick={handlePlaylistClick} />
             )}
-            {(!playlists || playlists.filter((playlist) => playlist.isPublic).length === 0) && (
-              <EmptyMessage Icon={HiOutlinePlay}>아직 공개된 플리가 없습니다</EmptyMessage>
-            )}
+            {!playlistsLoading &&
+              (!playlists || playlists.filter((playlist) => playlist.isPublic).length === 0) &&
+              currentUser?.uid !== userId && (
+                <EmptyMessage Icon={HiOutlinePlay}>아직 공개된 플리가 없습니다</EmptyMessage>
+              )}
           </TabContent>
           <TabContent id="likes" activeTabId={activeTab}>
             {loadingLikedPosts ? (
@@ -169,7 +175,7 @@ const ProfilePage: React.FC = () => {
             ) : (
               likedPosts.map((post) => <Post key={post.postId} post={post} id={post.postId} />)
             )}
-            {likedPosts.length === 0 && (
+            {!loadingLikedPosts && likedPosts.length === 0 && (
               <EmptyMessage Icon={HiOutlineHeart}>아직 좋아요 한 포스트가 없습니다</EmptyMessage>
             )}
           </TabContent>
